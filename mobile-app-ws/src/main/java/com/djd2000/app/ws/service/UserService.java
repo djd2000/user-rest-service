2e1602a0e18ce829ba6585a93df6145fd4e0e387
david <davidd125@yahoo.com>
2019-09-11 19:57:29 +0100

    Added user objects

---
+++ mobile-app-ws/src/main/java/com/djd2000/app/ws/service/UserService.java
@@ -0,0 +1,9 @@
+package com.djd2000.app.ws.service;
+
+import com.djd2000.app.ws.shared.dto.UserDto;
+
+public interface UserService {
+
+	public UserDto createUser(UserDto userDto);
+
+}
---
+++ mobile-app-ws/src/main/java/com/djd2000/app/ws/service/UserServiceImpl.java
@@ -0,0 +1,16 @@
+package com.djd2000.app.ws.service;
+
+import org.springframework.stereotype.Service;
+
+import com.djd2000.app.ws.shared.dto.UserDto;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+	@Override
+	public UserDto createUser(UserDto userDto) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
